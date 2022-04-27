302fd7cfb359e6d69aabfbb82eb562838ae2b033
Kirill Bubenchikov <63867291+bcd8697@users.noreply.github.com>
2022-04-27 15:14:25 +0300

    Create TwoSum.py

---
+++ neetcode-150/Arrays-and-Hashing/TwoSum.py
@@ -0,0 +1,14 @@
+'''
+https://leetcode.com/problems/two-sum/
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        prevMap = {}
+        
+        for i in range(len(nums)):
+            if target - nums[i] in prevMap:
+                return [prevMap[target - nums[i]], i]
+            else: 
+                prevMap[nums[i]] = i
